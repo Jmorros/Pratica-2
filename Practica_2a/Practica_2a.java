cea33c97f3ff72c0ca37e3edd62bb400ef758a15
Jmorros <morros_2468@hotmail.com>
2015-02-06 20:20:08 +0100

    Practica 2 exercici 1

---
+++ Practica_2a/Practica_2a.java
@@ -0,0 +1,30 @@
+import muvium.compatibility.arduino.Arduino; 
+class Practica_2a extends Arduino{//Automatically Added VBB Framework Code - do not remove
+/*************************************************************************  
+
+En aquesta practica provem la funcio print
+
+*************************************************************************/
+
+//*******  Includes  *****************************************************
+
+
+//*******  Variables  ****************************************************
+
+
+//*******  Setup  ********************************************************
+
+public void setup()
+{
+Serial.begin(9600);
+Serial.println("Hello World!!");
+}
+
+
+//*******  Loop  *********************************************************
+public void loop()
+{
+
+}
+
+}
