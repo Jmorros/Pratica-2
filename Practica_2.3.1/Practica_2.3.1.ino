38a180e120abefedf94f89bd40af2a43c3c8469d
Jmorros <morros_2468@hotmail.com>
2015-03-02 16:57:42 +0100

    Practica 2 exercici apartat 3 execici 1

---
+++ Practica_2.3.1/Practica_2.3.1.ino
@@ -0,0 +1,41 @@
+/*************************************************************************  
+**                                                                      **
+**                        Formats d'esciptura                           **                                               
+**                                                                      **
+*************************************************************************/
+
+//*******  Includes  *****************************************************
+
+
+//*******  Variables  ****************************************************
+int num = 64; 
+
+
+//*******  Setup  ********************************************************
+void setup(){
+  Serial.begin(9600);     // Comunicació a 9600bps
+  Serial.print("Diferents formats per al numero: "); 
+  Serial.println(num); 
+  
+  Serial.print("Decimal:");
+  Serial.println(num,DEC);  //El numero estara escrit amb decimal
+  
+  Serial.print("Binari:");
+  Serial.println(num,BIN);  //El numero estara escrit amb binari
+  
+  Serial.print("Hexadecimal:");
+  Serial.println(num,HEX);  //El numero estara escrit amb hexadecimal
+  
+  Serial.print("Octal:");
+  Serial.println(num,OCT);  //El numero estara escti amb octal
+  
+}
+
+
+//*******  Loop  *********************************************************
+void loop(){
+
+
+}
+
+
